e2e91f9f49787660a103314fda53d7eb73deaed3
Penguinang <1157703273@qq.com>
2018-05-25 23:10:32 +0800

    删除了暂时没用的依赖

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import find_packages
 from setuptools import setup
 
 install_requires = [
-    'numpy',
-    'imageio',
-    'matplotlib',
-    'scipy',
-    'scikit-image'
 ]
 
 tests_require = [
